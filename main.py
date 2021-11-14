0f50d9d5eb075cecd7a5d87007e6a7a1efe2fea3
Damian Anslik <45298102+damian-anslik@users.noreply.github.com>
2021-11-15 00:02:30 +0100

    Added comments

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import youtube_dl
 
 
 def download_audio(url: str, download_dir: str = None):
+    """  
+    Attempts to extract and download from the provided Youtube video URL
+
+    args:
+        - url : the URL of the video being downloaded
+        - download_dir : location in which to store the downloaded file (by default downloads)
+
+    returns:
+        - filename : the name of the downloaded file
+    """
     video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
     filename = "{download_dir}/{title}.mp3".format_map(
         {
-            "download_dir": download_dir,
+            "download_dir": download_dir if download_dir else "downloads",
             "title": video_info["title"],
         }
     )
@@ -26,15 +36,19 @@ def download_audio(url: str, download_dir: str = None):
 
 
 if __name__ == "__main__":
+    # Load script configuration file
     with open("config.json", "r") as config_file:
         config = json.load(config_file)
 
+    # Read urls from the text file
     with open(config["dataFile"], "r") as data_file:
         urls = data_file.read().splitlines()
 
+    # Loop over all urls contained in the text file
     for url in urls:
         downloaded_filename = download_audio(url, download_dir=config["downloadDir"])
         if downloaded_filename is not None:
+            # If the file is downloaded succesfully, check if the converted file dir exists, and use ffmpeg to convert to mp3 from m4a
             downloaded_filepath = os.path.join(
                 config["downloadDir"], downloaded_filename
             )
