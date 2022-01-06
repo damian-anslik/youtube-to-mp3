bccb9acf53ff8387e8e63d574b3f4e8578372fe2
damian-anslik <damiananslik@outlook.com>
2022-01-06 23:03:06 +0000

    Removed stale comment

--- main.py
+++ main.py
@@ -13,30 +13,3 @@ if __name__=="__main__":
         print(downloaded_file)
     else:
         download_audio_multiple(EXAMPLE_URLS, download_dir=DOWNLOAD_DIRECTORY)
-
-
-# if __name__ == "__main__":
-#     # Load script configuration file
-#     with open("config.json", "r") as config_file:
-#         config = json.load(config_file)
-
-#     # Read urls from the text file
-#     with open(config["dataFile"], "r") as data_file:
-#         urls = data_file.read().splitlines()
-
-#     # Loop over all urls contained in the text file
-#     for url in urls:
-#         downloaded_filename = download_audio(url, download_dir=config["downloadDir"])
-#         if downloaded_filename is not None:
-#             # If the file is downloaded succesfully, check if the converted file dir exists, and use ffmpeg to convert to mp3 from m4a
-#             downloaded_filepath = os.path.join(
-#                 config["downloadDir"], downloaded_filename
-#             )
-#             if not os.path.exists(config["convertedDir"]):
-#                 os.mkdir(config["convertedDir"])
-#             converted_filepath = os.path.join(
-#                 config["convertedDir"], downloaded_filename
-#             )
-#             os.system(
-#                 f"ffmpeg -i '{downloaded_filepath}' -c:v copy -c:a libmp3lame -q:a 4 '{converted_filepath}'"
-#             )
